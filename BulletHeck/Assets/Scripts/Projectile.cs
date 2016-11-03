6e746d4815f734e1a23e3b5380376e64df1ae98b
Gabriel Gray <Gabriel Gray>
2016-11-02 21:06:00 -0400

    with left and right walking sprites (uncut)

---
+++ BulletHeck/Assets/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	// This field would be private, but we want to view it in the Inspector
+	public WeaponType    _type;
+	SpriteRenderer rend;
+	public Rigidbody2D rb;
+
+	// This public property masks the field _type & takes action when it is set
+	public WeaponType    type {
+		get {
+			return( _type );
+		}
+		set {
+			SetType( value );
+		}
+	}
+
+	public void SetType( WeaponType eType ) {
+		// Set the _type
+		_type = eType;
+		WeaponDefinition def = Main.GetWeaponDefinition( _type );
+		rend.material.color = def.projectileColor;
+	}
+
+	void Awake(){
+		rend = GetComponent<SpriteRenderer> ();
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ BulletHeck/Assets/Scripts/Ship.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+    
+    // These fields control the movement of the ship
+    public float            speed = 30;
+    public float            rollMult = -45;
+    public float            pitchMult = 30;
+
+	// Declare a new delegate type WeaponFireDelegate
+	public delegate void WeaponFireDelegate();
+	// Create a WeaponFireDelegate field named fireDelegate.
+	public WeaponFireDelegate fireDelegate;
+
+    void Update () {
+    	// Pull in information from the Input class
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        
+        // Change transform.position basec on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;	
+        
+        // Rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0f);
+		
+		// Use the fireDelegate to fire Weapons
+		// First, make sure the button is pressed Axis("Jump")
+		// Then ensure that fireDelegate isn't null to avoid an error
+		if (Input.GetAxis("Jump") == 1 && fireDelegate != null) {
+			fireDelegate();
+		}
+
+    }
+	
+
+	
+}
